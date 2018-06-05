b75574f7f5223a5cda13c397e1b51f121920ac80
Adriean Khisbe <adriean.khisbe@live.fr>
2018-06-05 11:25:06 +0200

    Initial Prototype of slack lambda

--- aws-slack-codepipeline-watch.js
+++ aws-slack-codepipeline-watch.js
@@ -1,5 +1,27 @@
+const { WebClient } = require('@slack/client');
+
+const token = process.env.SLACK_TOKEN;
+if (!token) throw new Error('Need a valid token defined in SLACK_TOKEN');
+
+const channel = process.env.SLACK_CHANNEL;
+if (!channel) throw new Error('Need a valid chanel defined in SLACK_CHANNEL');
+
+const web = new WebClient(token);
+
+const EVENT_TYPES = {
+    pipeline: 'CodePipeline Pipeline Execution State Change',
+    stage: 'CodePipeline Stage Execution State Change',
+    action: 'CodePipeline Action Execution State Change'
+};
+
 
 exports.handler = (event, context, callback) => {
-    console.log(event);
-    callback(null, 'Acknoledge Event');
+    if (event.source !== 'aws.codepipeline')
+        return callback(new Error(`Called from wrong source ${event.source}`));
+
+    web.chat.postMessage({ channel, text: `${event.detail.pipeline} : ${event.detail.state}` })
+        .then(res => {
+            callback(null, 'Acknoledge Event');
+        }).catch(err => callback(err));
+
 };
