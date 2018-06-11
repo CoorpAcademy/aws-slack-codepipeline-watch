12cd439e13bc42869b8c377b5686f97f1406cc43
Adriean Khisbe <adriean.khisbe@live.fr>
2018-06-11 07:50:52 +0200

    Made a script to easily generate release zip

---
+++ make-release-zip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+current_commit="$(git rev-parse --short HEAD)"
+zipfile="aws-slack-codepipeline-watch-release-${1:-$current_commit}.zip"
+echo ">> Installing node_module production"
+rm -rf node_modules
+NODE_ENV=production npm -q i 
+
+echo ">> Generating Zip file"
+zip "$zipfile" node_modules aws-slack-codepipeline-watch.js
+
+echo ">> Reinstalling noode_module with dev dependencies"
+npm -q i
+echo 
+echo ">> Zip available: $zipfile"
