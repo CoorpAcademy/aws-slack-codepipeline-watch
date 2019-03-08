cc7fc9d942f2c5e2133d3cfd870c05b89aeb1adf
Adriean Khisbe <adriean.khisbe@live.fr>
2019-03-08 17:06:52 +0100

    Boost make release script with dependencies cache :rocket:

--- scripts/make-release-zip.sh
+++ scripts/make-release-zip.sh
@@ -4,13 +4,14 @@ mkdir -p release
 current_commit="$(git rev-parse --short HEAD)"
 zipfile="release/aws-slack-codepipeline-watch-release-${1:-$current_commit}.zip"
 echo ">> Installing node_module production"
-rm -rf node_modules
+mv node_modules node_modules.bak
 NODE_ENV=production npm -q i 
 
 echo ">> Generating Zip file"
 zip -r -q "$zipfile" node_modules lambda
 
-echo ">> Reinstalling noode_module with dev dependencies"
-npm -q i
+echo ">> Restoring noode_module with dev dependencies"
+rm -rf node_modules
+mv node_modules.bak node_modules
 echo 
 echo ">> Zip available: $zipfile"
