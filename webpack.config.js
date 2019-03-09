ddffff94b860514f7ceb326e7e5408cb8fe1fffb
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-03-09 09:23:46 +0100

    Set up serverless-webpack :cyclone:
    special dedicace to @godu :wink:

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false
+  },
+  performance: {
+    // Turn off size warnings for entry points
+    hints: false
+  },
+  devtool: slsw.lib.webpack.isLocal ? 'inline-cheap-module-source-map' : 'nosources-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.yaml$/,
+        use: ['json-loader', 'yaml-loader']
+      }
+    ]
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map'
+  },
+  externals: {
+    'aws-sdk': 'aws-sdk'
+  }
+};
