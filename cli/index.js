7016d6677316159e9dde735aa8a845edaf5d24da
Adriean Khisbe <adriean.khisbe@live.fr>
2019-03-13 08:27:29 +0100

    Refactor initial script using spwan rather than exec :hammer_and_pick:

--- cli/index.js
+++ cli/index.js
@@ -1,12 +1,19 @@
-const {exec} = require('child_process');
+const {spawn} = require('child_process');
 const path = require('path');
 
 const SERVERLESS = path.join(__dirname, '../node_modules/.bin/serverless');
 
+const deploy = cb => {
+   const sls = spawn(SERVERLESS, ['package']);
+   sls.stdout.on('data', output => console.log(output.toString()))
+   sls.stderr.on('data', output => console.error(output.toString()))
+   sls.on('close', exitCode => {
+       if (!cb) return;
+       if (exitCode === 0) return cb();
+       return cb(new Error(`Some error occured, exitCode:${exitCode}`))
+   })
+};
+
 if (!module.parent) {
-  exec(`${SERVERLESS} package`,(err, stdout, stderr) => {
-      console.log(err)
-      console.log(stdout)
-      console.error(stderr)
-  })
+    deploy();
 }
