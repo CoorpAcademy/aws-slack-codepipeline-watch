412e4fe4f008d1c0bd491470404528b7b2c904a0
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-12-18 18:42:18 +0100

    Replace deprectaed @slack/client by @slack/web-api :recycle:

--- lambda/aws-slack-codepipeline-watch.js
+++ lambda/aws-slack-codepipeline-watch.js
@@ -1,4 +1,4 @@
-const {WebClient} = require('@slack/client');
+const {WebClient} = require('@slack/web-api');
 const AWS = require('aws-sdk');
 const request = require('request');
 const _ = require('lodash/fp');
